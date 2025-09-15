894c9783d8baaa0647ad7072a5c1dc699e036e24
eliewebavenue <elie@webavenue.agency>
2025-09-15 15:00:19 +0200

    Modification des routes pour les monuments : changement de l'URL de '/monument/:id' à '/monuments/:id' et ajout d'une nouvelle route '/monuments' pour récupérer la liste des monuments avec des réponses JSON formatées.

--- server.js
+++ server.js
@@ -6,11 +6,20 @@ app.get('/', (req, res) => {
     res.send('Hello, Express !')
 })
 
-app.get('/monument/:id', (req, res) => {
+app.get('/monuments/:id', (req, res) => {
     const id = parseInt(req.params.id)
     const monument = monuments.find(m => m.id === id)
-    res.send(`${monument.name}`)
+    const message = `Le monument avec l'ID ${id} a bien été trouvé.`;
+    const data = monument
+    res.json({message, data})
 })
+
+app.get('/monuments', (req, res) => {
+    const message = 'La liste des monuments a bien été récupérée.'
+    const data = monuments
+    res.json({message, data})
+})
+
 
 
 app.listen(3000, () => console.log('Server running at http://localhost:3000'))
