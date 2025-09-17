3159680b65bf1dbcf3c1c9045fa18e3779ae519b
eliewebavenue <elie@webavenue.agency>
2025-09-17 12:21:05 +0200

    Refactor la création de monuments pour utiliser async/await et ajouter une gestion des erreurs centralisée

---
+++ helper.js
@@ -0,0 +1,6 @@
+exports.handleError = (res, error, message) => {
+    if(error.name === 'SequelizeValidationError'){
+        return res.status(400).json({ message, data: error.errors.map(e => e.message) });
+    }
+    res.status(500).json({message : "Une erreur s'est produite. Réessayez dans quelques instants.", data: null });
+};
--- src/routes/createMonument.route.js
+++ src/routes/createMonument.route.js
@@ -1,20 +1,20 @@
 const { MonumentModel } = require('../db/sequelize');
+const { handleError } = require('../helper');
 
 module.exports = (app) => {
-    app.post('/monuments', (req, res) => {
-        MonumentModel.create(req.body)
-            .then(monument => {
-                const message = `Le monument ${monument.title} a bien été créé.`
-                res.json({ message, data: monument })
-            })
-            .catch(error => {
-                if(error.name === "SequelizeValidationError"){  
-                    const validationErrors = error.errors.map(e => e.message);
-                    const message = "Le monument n'a pas pu être créé. Vérifiez les informations saisies.";
-                    return res.status(400).json({ message, data: validationErrors });
-                }
-                const message = "Le monument n'a pas pu être créé. Réessayez dans quelques instants."
-                return res.status(500).json({ message, data: error })
-            });
+    app.post('/monuments', async (req, res) => {
+        const { monument } = req.body;
+
+        try {
+
+            const createdMonument = await MonumentModel.create(monument);
+
+            const message = `Le monument ${createdMonument.title} a bien été créé.`;
+            res.status(201).json({ message, data: createdMonument });
+
+        } catch (error) {
+            const message = "Le monument n'a pas pu être créé. Réessayez dans quelques instants.";
+            return handleError(res, error, message);
+        }
     });
 }   
