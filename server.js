2dc2e24df335535b1534a0c3a54d76373a65d0a7
eliewebavenue <elie@webavenue.agency>
2025-09-18 16:36:33 +0200

    Refactor la gestion de Socket.io en déplaçant la logique dans un fichier séparé et en supprimant le code redondant dans server.js

--- server.js
+++ server.js
@@ -4,69 +4,12 @@ const morgan = require('morgan')
 const sequelize = require('./src/db/sequelize')
 const auth = require('./src/auth/auth')
 const http = require('http');
-const fs = require('fs');
-const jwt = require('jsonwebtoken');
-const { Server } = require('socket.io');
-const publicKey = fs.readFileSync('./src/auth/jwtRS256.key.pub');
+const setupSocketServer = require('./src/socket');
+
 
 const app = express()
 const server = http.createServer(app);
-
-const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
-});
-
-io.use((socket, next) => {
-    const token = socket.handshake.auth?.token;
-    if (!token) {
-        return next(new Error("Token manquant"));
-    }
-
-    jwt.verify(token, publicKey, { algorithms: ['RS256'] }, (err, decoded) => {
-        if (err) {
-            return next(new Error("Token invalide"));
-        }
-        socket.user = decoded;
-        next();
-    }); 
-});
-
-let messages = {};
-
-io.on('connection', (socket) => {
-    console.log('Un utilisateur est connecté');
-
-    socket.on("joinMonument", ({monumentId, role}) => {
-        socket.join(`monument_${monumentId}`);
-        console.log(`${socket.user.userName} a rejoint la salle monument_${monumentId} en tant que ${role}`);
-
-        if(!messages[monumentId]) 
-            messages[monumentId] = [];
-
-        socket.emit("chatHistory", messages[monumentId]);
-    });
-
-    socket.on("sendMessage", ({monumentId, role, message}) => {
-        const msg = {
-            user: socket.user.userName,
-            role,
-            message,
-            date: new Date()
-        };
-
-        messages[monumentId].push(msg);
-        io.to(`monument_${monumentId}`).emit("newMessage", msg);
-    });
-    
-    socket.on('disconnect', () => {
-        console.log('Un utilisateur est déconnecté');
-    });
-});
-
-
+setupSocketServer(server);
 
 function nightBlocker (req, res, next){
     const hour = new Date().getHours();
